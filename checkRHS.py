41559013107949ff441126fc9dc9452bf4d0a321
Joshua <jwhite88@gmail.com>
2024-07-25 18:28:50 +1000

    Bugfixes as per pylint

--- checkRHS.py
+++ checkRHS.py
@@ -15,50 +15,52 @@ import pandas as pd
 import nga # Custom module for NGA and other resources
 
 def genusName(abbrev):
+	'''Convert genus abbreviation into full name.'''
+
 	if abbrev == 'Cym.':
 		return 'Cymbidium'
-	else:
-		return abbrev
+
+	return abbrev
 
 
 def extractName(rhs_obj, plant):
 	'''Parse a plant name to extract genus and grex.'''
-	
+
 	reg = True
-	
+
 	# Extract genus
 	genus = ''
-	matches = re.search('^\w{1,5}\.', plant, flags=re.IGNORECASE)
+	matches = re.search(r'^\w{1,5}\.', plant, flags=re.IGNORECASE)
 	if matches is not None:
 		genus = matches.group()
 		plant = plant[len(genus):].strip()
 
 	# Remove any ploidy references
-	matches = re.search('\(.*\dn\)', plant, flags=re.IGNORECASE)
+	matches = re.search(r'\(.*\dn\)', plant, flags=re.IGNORECASE)
 	if matches is not None:
 		ploidy = matches.group()
 		plant = plant.replace(ploidy, '').replace('  ',' ').strip()
 
 	# Remove numbered selections
-	matches = re.search(' #\d+?', plant, flags=re.IGNORECASE)
+	matches = re.search(r' #\d+?', plant, flags=re.IGNORECASE)
 	if matches is not None:
 		selection = matches.group()
 		plant = plant.replace(selection, '').replace('  ',' ').strip()
 
 	# Remove named selections
-	matches = re.search(' \'.+\'$', plant, flags=re.IGNORECASE)
+	matches = re.search(r' \'.+\'$', plant, flags=re.IGNORECASE)
 	if matches is not None:
 		selection = matches.group()
 		plant = plant.replace(selection, '').replace('  ',' ').strip()
 
 	# Remove forms
-	matches = re.search(' f. \w+', plant, flags=re.IGNORECASE)
+	matches = re.search(r' f. \w+', plant, flags=re.IGNORECASE)
 	if matches is not None:
 		selection = matches.group()
 		plant = plant.replace(selection, '').replace('  ',' ').strip()
 
 	# Check if this plant is also an unnamed cross
-	matches = re.findall(' x ', plant, flags=re.IGNORECASE)
+	matches = re.findall(r' x ', plant, flags=re.IGNORECASE)
 	if matches is not None and len(matches) > 0:
 		if len(matches) > 1:
 			print("WARNING: Hybrid parent detected")
@@ -84,7 +86,7 @@ def checkRegistration(rhs_obj, pod_parent, pollen_parent):
 	if pod['registered'] and pol['registered']:
 		pod_genus = genusName(pod['genus'])
 		pollen_genus = genusName(pol['genus'])
-		req = my_rhs.searchParentage(pod_genus,pod['grex'],pollen_genus,pol['grex'],True)
+		req = rhs_obj.searchParentage(pod_genus,pod['grex'],pollen_genus,pol['grex'],True)
 	else:
 		req = {'matched':False}
 
@@ -95,15 +97,15 @@ def checkRegistration(rhs_obj, pod_parent, pollen_parent):
 if __name__ == '__main__':
 
 	# TODO: Make this use argparse
-	fname = 'crosses.csv'
+	FNAME = 'crosses.csv'
 
 	# Check if the source file exists
-	if not os.path.isfile(fname):
-		print("%s not found" % fname)
+	if not os.path.isfile(FNAME):
+		print("%s not found" % FNAME)
 		sys.exit(1)
 
 	# Read the source CSV
-	df = pd.read_csv(fname)
+	df = pd.read_csv(FNAME)
 	df.drop_duplicates(inplace=True)
 
 	# Connect to the RHS
@@ -111,7 +113,6 @@ if __name__ == '__main__':
 	my_rhs.dbConnect('RHS.db')
 
 	# Iterate through the source CSV
-	n = 0
 	for idx, row in df.iterrows():
 		results = checkRegistration(my_rhs, row['Pod Parent'], row['Pollen Parent'])
 		if results is not None:
@@ -126,4 +127,3 @@ if __name__ == '__main__':
 			print("Error")
 
 	my_rhs.dbClose()
-	sys.exit()
